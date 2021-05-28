43dd3be6587867f93b6cf10a840d47afb8ae4ce7
vineeth-pappu <vineeth0394@gmail.com>
2021-05-28 22:19:00 +0530

    Implement Search notes

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,27 @@ notesListWapper.innerHTML = cards.join("");
 
 const createNoteComponent = CreateNoteComponent()
 createNoteComponent.render('create-notes-wrapper')
+
+const searchInput = document.getElementById('search')
+searchInput.addEventListener('input', function searchNotes(e) {
+    const searchValue = e.target.value
+    
+    if (searchValue) {
+        const cards = notes.filter(n => {
+            if (
+                n.title.includes(searchValue) ||
+                n.body.includes(searchValue)
+            ) {
+                return true
+            } else {
+                return false
+            }
+        }).map(note => NotesCardComponent(note))
+        notesListWapper.innerHTML = cards.join("");
+        
+    } else {
+        const cards = notes.map(note => NotesCardComponent(note))
+        notesListWapper.innerHTML = cards.join("");
+    }
+
+})
