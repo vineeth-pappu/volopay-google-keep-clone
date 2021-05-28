a9aecf997b91eae374681a10313690f7b5a42814
vineeth-pappu <vineeth0394@gmail.com>
2021-05-28 22:28:34 +0530

    Refactor: create Search Notes Input component

--- scripts/main.js
+++ scripts/main.js
@@ -8,26 +8,5 @@ notesListWapper.innerHTML = cards.join("");
 const createNoteComponent = CreateNoteComponent()
 createNoteComponent.render('create-notes-wrapper')
 
-const searchInput = document.getElementById('search')
-searchInput.addEventListener('input', function searchNotes(e) {
-    const searchValue = e.target.value
-    
-    if (searchValue) {
-        const cards = notes.filter(n => {
-            if (
-                n.title.includes(searchValue) ||
-                n.body.includes(searchValue)
-            ) {
-                return true
-            } else {
-                return false
-            }
-        }).map(note => NotesCardComponent(note))
-        notesListWapper.innerHTML = cards.join("");
-        
-    } else {
-        const cards = notes.map(note => NotesCardComponent(note))
-        notesListWapper.innerHTML = cards.join("");
-    }
-
-})
+const notesSearchInput = NotesSearchInput()
+notesSearchInput.render('search-bar')
